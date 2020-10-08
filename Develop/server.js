b2be502628efaa45ec7e21ce55c79a4cfe03a44a
Ryan Mendez <rjmendez80@gmail.com>
2020-10-08 12:17:40 -0700

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+// setting up express
 const app = express();
 const PORT = process.env.PORT || 3003;
 
@@ -11,9 +12,9 @@ const PORT = process.env.PORT || 3003;
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
-//routes
+//html routes starting
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -23,7 +24,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
+//api routes
+
 app.get('api/notes',(req, res) => {
+    let rawData =
     res.send('notes')
 });
 
