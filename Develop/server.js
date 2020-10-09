eb1c80ff74dd23210842fed2811ea9a844a15cc4
Ryan Mendez <rjmendez80@gmail.com>
2020-10-08 17:51:12 -0700

    updated jb.json and debugging app.post in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -22,18 +22,36 @@ app.get('/', (req, res) => {
 });
 // displays the notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 //api routes
+
 // sends the notes to the jb.json file
 app.get('api/notes',(req, res) => {
-    fs.readFile(path.join(__dirname, '../db/db.json'), (err, notes) => {
-        if (err) throw err;
-        res.send(JSON.parse(notes))
-    });
+    const note = JSON.parse(fs.readFileSync( './db/db.json', 'utf8'))
+    res.JSON(note)
 });
 
+
+app.post('api/notes', (req, res) => {
+    // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+    const newNote = req.body;
+    const dbNote = JSON.parse(fs.readFileSync( './db/db.json', 'utf8'));
+    const noteLength = dbNote.length > 0 ? dbNote[dbNote.length -1] : null;
+    const id = noteLength !== null ? noteLength.id+1 : 1;
+    // add it to array then return to client
+    const newDb = dbNote.push(...newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(newDb));
+    return res.send(true);
+})
+
+app.delete('api/notes/:id', (req, res) => {
+    // Should receive a query parameter containing the id of a note to delete. T
+    // his means you'll need to find a way to give each note a unique `id` when it's saved.
+    // In order to delete a note, you'll need to read all notes from the `db.json` file,
+    // remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+})
 
 
 app.listen(PORT, () => console.log(`hear you on port ${PORT}`));
